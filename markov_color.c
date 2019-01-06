cfacf34e06edd77df78ff478efa4dc658dbb8f8a
Andrey Portnoy <aportnoy@ucsd.edu>
2019-01-06 01:29:24 -0800

    Write down some ideas.

--- markov_color.c
+++ markov_color.c
@@ -10,6 +10,11 @@
  * 2. What if we start in the center?
  * 3. Is unsigned char the best type for RGB stuff? Yes!
  * 4. What if evolve only a randomly chosen component of RGB?
+ * 5. RGB space is a cube. We can send vectors in brownian motion inside that
+ *    box. But colors pleasant to the eye may form a boxy connected subset of
+ *    RGB space. Can we send a vector in brownian motion inside such a subset?
+ * 6. Can we make GIFs by evolving not rows but whole images? So a pixel has 8
+ *    parents, a kernel?
  */
 
 /**
