1abddbd05b04973dd2f6ce077d427c1e0409ea53
Andrey Portnoy <aportnoy@ucsd.edu>
2019-01-07 01:26:34 -0800

    Switch to clang and add image_by_image stub.

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-CC=gcc
+CC=clang
 CFLAGS=-Wall -Wextra -ansi -pedantic
+
+image_by_image: image_by_image.c image.o evolve.o
+	$(CC) $(CFLAGS) -o image_by_image image_by_image.c image.o evolve.o
 
 row_by_row: row_by_row.c image.o evolve.o
 	$(CC) $(CFLAGS) -o row_by_row row_by_row.c image.o evolve.o
@@ -11,4 +14,4 @@ evolve.o: evolve.c evolve.h
 	$(CC) $(CFLAGS) -c -o evolve.o evolve.c
 
 clean:
-	rm -rf row_by_row *.o *.ppm *.png *.dSYM
+	rm -rf image_by_image row_by_row *.o *.ppm *.png *.dSYM
---
+++ image_by_image.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "image.h"
+
+#define N_IMAGES 100
+#define WIDTH 1000
+#define HEIGHT 1000
+#define FILENAME_LENGTH 100
+
+void write_images(Image **images, size_t n);
+
+void write_images(Image **images, size_t n) {
+    size_t i;
+
+    for (i = 0; i < n; ++i) {
+        char filename[FILENAME_LENGTH];
+        FILE *file;
+        sprintf(filename,  "images/random%lu.ppm", i);
+        file = fopen(filename, "w");
+        if (file) {
+            write_image(file, images[i]);
+            fclose(file);
+        } else {
+            fprintf(stderr, "Failed to open file %s\n", filename);
+            exit(1);
+        }
+    }
+}
+
+int main() {
+    Image *images[N_IMAGES];
+    int i;
+
+    for (i = 0; i < N_IMAGES; ++i) {
+        images[i] = make_random_image(WIDTH, HEIGHT);
+    }
+
+    write_images(images, N_IMAGES);
+}
