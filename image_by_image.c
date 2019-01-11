69e68387e567f116a2f1be42c0e7f9cfe9ea001c
Andrey Portnoy <aportnoy@ucsd.edu>
2019-01-10 18:11:31 -0800

    Update image_by_image runner with new evolvers and prints.

--- image_by_image.c
+++ image_by_image.c
@@ -1,39 +1,57 @@
-#include <assert.h>
-#include <stdio.h>
 #include "image.h"
+#include <assert.h>
+#include <limits.h>
+#include <stdio.h>
+#include "evolve.h"
 
-#define N_IMAGES 100
-#define WIDTH 1000
-#define HEIGHT 1000
+#define N_IMAGES 200
+#define WIDTH 200
+#define HEIGHT 200
 #define FILENAME_LENGTH 100
 
-void write_images(Image **images, size_t n);
+void write_images(Image **images, int n);
+Image **generate_images(void);
 
-void write_images(Image **images, size_t n) {
-    size_t i;
+void write_images(Image **images, int n) {
+    int i;
 
     for (i = 0; i < n; ++i) {
         char filename[FILENAME_LENGTH];
         FILE *file;
-        sprintf(filename,  "images/random%lu.ppm", i);
+        sprintf(filename, "images/random%d.ppm", i);
         file = fopen(filename, "w");
         if (file) {
             write_image(file, images[i]);
             fclose(file);
+            printf("\33[2K\rSaved image %d...", i);
+            fflush(stdout);
         } else {
             fprintf(stderr, "Failed to open file %s\n", filename);
             exit(1);
         }
     }
+    printf("\33[2K\rDone saving.\n");
+}
+
+Image **generate_images() {
+    Image **images;
+    int i;
+
+    images = malloc(N_IMAGES * sizeof(*images));
+
+    images[0] = make_random_image(WIDTH, HEIGHT);
+    for (i = 1; i < N_IMAGES; ++i) {
+        images[i] = make_image(WIDTH, HEIGHT);
+        evolve_image_4_parent_genes(images[i], images[i-1]);
+        printf("\33[2K\rGenerated image %d...", i);
+        fflush(stdout);
+    }
+    printf("\33[2K\rDone generating.\n");
+    fflush(stdout);
+    return images;
 }
 
 int main() {
-    Image *images[N_IMAGES];
-    int i;
-
-    for (i = 0; i < N_IMAGES; ++i) {
-        images[i] = make_random_image(WIDTH, HEIGHT);
-    }
-
+    Image **images = generate_images();
     write_images(images, N_IMAGES);
 }
